16c5ac97428f3527b598765888ff25b1f67ccfa4
Søren Trudsø Mahon <soren.mahon@m10s.io>
2026-05-05 15:48:10 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:TestedTrek/StarTrek/Game.cs

--- TestedTrek/StarTrek/Game.cs
+++ TestedTrek/StarTrek/Game.cs
@@ -25,59 +25,70 @@ public class Game {
     }
 
     public void FireWeapon(Galaxy wg) {
-        if (wg.Parameter("command").Equals("phaser")) {
-			int amount = int.Parse(wg.Parameter("amount"));
-			Klingon enemy = (Klingon) wg.Variable("target");
-			if (e >= amount) {
-				int distance = enemy.Distance();
-				if (distance > 4000) {
-					wg.WriteLine("Klingon out of range of phasers at " + distance + " sectors...");
-				} else {
-					int damage = amount - (((amount /20)* distance /200) + Rnd(200));
-					if (damage < 1)
-						damage = 1;
-					wg.WriteLine("Phasers hit Klingon at " + distance + " sectors with " + damage + " units");
-					if (damage < enemy.GetEnergy()) {
-						enemy.SetEnergy(enemy.GetEnergy() - damage);
-						wg.WriteLine("Klingon has " + enemy.GetEnergy() + " remaining");
-					} else {
-						wg.WriteLine("Klingon destroyed!");
-						enemy.Delete();
-					}
-				}
-				e -= amount;
+        if (wg.Parameter("command").Equals("phaser"))
+        {
+	        Phaser(wg);
+        } else if (wg.Parameter("command").Equals("photon"))
+        {
+	        Photon(wg);
+        }
+	}
 
+	private void Photon(Galaxy wg)
+	{
+		Klingon enemy = (Klingon) wg.Variable("target");
+		if (t  > 0) {
+			int distance = enemy.Distance();
+			if ((Rnd(4) + ((distance / 500) + 1) > 7)) {
+				wg.WriteLine("Torpedo missed Klingon at " + distance + " sectors...");
 			} else {
-				wg.WriteLine("Insufficient energy to fire phasers!");
-			}
-
-		} else if (wg.Parameter("command").Equals("photon")) {
-			Klingon enemy = (Klingon) wg.Variable("target");
-			if (t  > 0) {
-				int distance = enemy.Distance();
-				if ((Rnd(4) + ((distance / 500) + 1) > 7)) {
-					wg.WriteLine("Torpedo missed Klingon at " + distance + " sectors...");
+				int damage = 800 + Rnd(50);
+				wg.WriteLine("Photons hit Klingon at " + distance + " sectors with " + damage + " units");
+				if (damage < enemy.GetEnergy()) {
+					enemy.SetEnergy(enemy.GetEnergy() - damage);
+					wg.WriteLine("Klingon has " + enemy.GetEnergy() + " remaining");
 				} else {
-					int damage = 800 + Rnd(50);
-					wg.WriteLine("Photons hit Klingon at " + distance + " sectors with " + damage + " units");
-					if (damage < enemy.GetEnergy()) {
-						enemy.SetEnergy(enemy.GetEnergy() - damage);
-						wg.WriteLine("Klingon has " + enemy.GetEnergy() + " remaining");
-					} else {
-						wg.WriteLine("Klingon destroyed!");
-						enemy.Delete();
-					}
+					wg.WriteLine("Klingon destroyed!");
+					enemy.Delete();
 				}
-				t -= 1;
-
-			} else {
-				wg.WriteLine("No more photon torpedoes!");
 			}
+			t -= 1;
+
+		} else {
+			wg.WriteLine("No more photon torpedoes!");
+		}
+	}
+
+	private void Phaser(Galaxy wg)
+	{
+		int amount = int.Parse(wg.Parameter("amount"));
+		Klingon enemy = (Klingon) wg.Variable("target");
+		if (e >= amount) {
+			int distance = enemy.Distance();
+			if (distance > 4000) {
+				wg.WriteLine("Klingon out of range of phasers at " + distance + " sectors...");
+			} else {
+				int damage = amount - (((amount /20)* distance /200) + Rnd(200));
+				if (damage < 1)
+					damage = 1;
+				wg.WriteLine("Phasers hit Klingon at " + distance + " sectors with " + damage + " units");
+				if (damage < enemy.GetEnergy()) {
+					enemy.SetEnergy(enemy.GetEnergy() - damage);
+					wg.WriteLine("Klingon has " + enemy.GetEnergy() + " remaining");
+				} else {
+					wg.WriteLine("Klingon destroyed!");
+					enemy.Delete();
+				}
+			}
+			e -= amount;
+
+		} else {
+			wg.WriteLine("Insufficient energy to fire phasers!");
 		}
 	}
 
 
-    // note we made generator public in order to stub it in tests;
+	// note we made generator public in order to stub it in tests;
     // it's ugly, but it's telling us something about our *design!* ;-)
 	public static Random generator = new Random();
 	internal static int Rnd(int maximum) {
