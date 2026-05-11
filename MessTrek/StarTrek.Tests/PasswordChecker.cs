3ee995c8b2b118319db91f076b3bf716dda23673
Michel Grootjans <michel.grootjans@gmail.com>
2026-05-11 14:37:31 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:MessTrek/StarTrek.Tests/PasswordCheckerTests.cs

---
+++ MessTrek/StarTrek.Tests/PasswordChecker.cs
@@ -0,0 +1,31 @@
+namespace StarTrek.Tests;
+
+public static class PasswordChecker
+{
+    public static PasswordStrength Check(string password)
+    {
+        return PasswordChecker2(password).Verdict;
+    }
+
+    public static (PasswordStrength Verdict, string[] Reasons) PasswordChecker2(string password, PasswordRequirements requirements = PasswordRequirements.Standard)
+    {
+        List<string> reasons = [];
+        var passwordLength = requirements == PasswordRequirements.Admin ? 10 : 7;
+
+        if (password.Length <= passwordLength) reasons.Add("To short, hon!");
+
+        if (DoesNotHave(password, char.IsDigit)) reasons.Add("N0 numb3rzzz!");
+        if (DoesNotHave(password, char.IsLetter)) reasons.Add("Missing 4lph4num3ric bro0!");
+        var passwordStrength = reasons.Any() ? PasswordStrength.Weak : PasswordStrength.Strong;
+        return (passwordStrength, [..reasons]);
+    }
+
+    public enum PasswordStrength
+    {
+        Weak,
+        Strong
+    }
+
+    private static bool DoesNotHave(string password, Func<char, bool> isDigit)
+        => password.Any(isDigit) is false;
+}
--- MessTrek/StarTrek.Tests/PasswordCheckerTests.cs
+++ MessTrek/StarTrek.Tests/PasswordCheckerTests.cs
@@ -9,20 +9,20 @@ public class PasswordCheckerTests
     private const string StrongAdminPassword = "123456789a!";
 
     [Theory]
-    [InlineData(PasswordStrength.Weak, BreaksDigitRule)]
-    [InlineData(PasswordStrength.Strong, StrongNoneAdminPassword)]
-    [InlineData(PasswordStrength.Weak, BreaksLengthRule)]
-    [InlineData(PasswordStrength.Weak, "")]
-    [InlineData(PasswordStrength.Weak, BreaksCharacterRule)]
-    public void CheckWeakPassword_MissingDigit(PasswordStrength expected, string input)
+    [InlineData(PasswordChecker.PasswordStrength.Weak, BreaksDigitRule)]
+    [InlineData(PasswordChecker.PasswordStrength.Strong, StrongNoneAdminPassword)]
+    [InlineData(PasswordChecker.PasswordStrength.Weak, BreaksLengthRule)]
+    [InlineData(PasswordChecker.PasswordStrength.Weak, "")]
+    [InlineData(PasswordChecker.PasswordStrength.Weak, BreaksCharacterRule)]
+    public void CheckWeakPassword_MissingDigit(PasswordChecker.PasswordStrength expected, string input)
     {
-        Assert.Equal(expected, PasswordChecker(input));
+        Assert.Equal(expected, PasswordChecker.Check(input));
     }
-    
+
     /*
-     *      
+     *
        £ Some clients want to be able to pass a Boolean “Admin” flag
-       
+
        to the API and, if true, the password must also…
        £ Be > 10 characters long.
        £ Contain a special character.
@@ -32,81 +32,45 @@ public class PasswordCheckerTests
     [Fact]
     public void PasswordChecker2_Simple_Weak()
     {
-        var passwordChecker2 = PasswordChecker2("");
-        
-        Assert.Equal(PasswordStrength.Weak, passwordChecker2.Verdict);
+        var passwordChecker2 = PasswordChecker.PasswordChecker2("");
+
+        Assert.Equal(PasswordChecker.PasswordStrength.Weak, passwordChecker2.Verdict);
     }
-    
+
     [Theory]
     [InlineData(BreaksLengthRule, "To short, hon!")]
     [InlineData(BreaksDigitRule, "N0 numb3rzzz!")]
     [InlineData(BreaksCharacterRule, "Missing 4lph4num3ric bro0!")]
     public void PasswordChecker2_BreaksLengthRule_Weak(string password, string expectedReason)
     {
-        var passwordChecker2 = PasswordChecker2(password);
-        
+        var passwordChecker2 = PasswordChecker.PasswordChecker2(password);
+
         Assert.Equal([expectedReason], passwordChecker2.Reasons);
     }
-    
+
     [Fact]
     public void PasswordChecker2_Strong_Strong()
     {
-        var passwordChecker2 = PasswordChecker2(StrongNoneAdminPassword);
-        
-        Assert.Equal(PasswordStrength.Strong, passwordChecker2.Verdict);
+        var passwordChecker2 = PasswordChecker.PasswordChecker2(StrongNoneAdminPassword);
+
+        Assert.Equal(PasswordChecker.PasswordStrength.Strong, passwordChecker2.Verdict);
     }
 
     [Fact]
     public void PasswordChecker2_Strong_Admin_Strong()
     {
-        var passwordChecker2 = PasswordChecker2(StrongAdminPassword, PasswordRequirements.Admin);
-        
-        Assert.Equal(PasswordStrength.Strong, passwordChecker2.Verdict);
+        var passwordChecker2 = PasswordChecker.PasswordChecker2(StrongAdminPassword, PasswordRequirements.Admin);
+
+        Assert.Equal(PasswordChecker.PasswordStrength.Strong, passwordChecker2.Verdict);
     }
-    
+
     [Fact]
     public void PasswordChecker2_Strong_Admin_Weak()
     {
-        var passwordChecker2 = PasswordChecker2("12345678a!", PasswordRequirements.Admin);
-        
-        Assert.Equal(PasswordStrength.Weak, passwordChecker2.Verdict);
+        var passwordChecker2 = PasswordChecker.PasswordChecker2("12345678a!", PasswordRequirements.Admin);
+
+        Assert.Equal(PasswordChecker.PasswordStrength.Weak, passwordChecker2.Verdict);
     }
-    
-    
-
-
-
-    private PasswordStrength PasswordChecker(string password)
-    {
-        return PasswordChecker2(password).Verdict;
-    }
-
-    private (PasswordStrength Verdict, string[] Reasons) PasswordChecker2(string password, PasswordRequirements requirements = PasswordRequirements.Standard)
-    {
-        List<string> reasons = [];
-        if (requirements == PasswordRequirements.Admin)
-        {
-            if (password.Length <= 10) reasons.Add("To short, hon!");
-        }
-        else
-        {
-            if (password.Length <= 7) reasons.Add("To short, hon!");
-        }
-
-        if(DoesNotHave(password, char.IsDigit)) reasons.Add("N0 numb3rzzz!");
-        if(DoesNotHave(password, char.IsLetter)) reasons.Add("Missing 4lph4num3ric bro0!");
-        var passwordStrength = reasons.Any() ? PasswordStrength.Weak : PasswordStrength.Strong;
-        return (passwordStrength, [..reasons]);
-    }
-
-
-    public enum PasswordStrength
-    {
-        Weak, Strong
-    }
-
-    private static bool DoesNotHave(string password, Func<char, bool> isDigit) 
-        => password.Any(isDigit) is false;
 }
 
 public enum PasswordRequirements
