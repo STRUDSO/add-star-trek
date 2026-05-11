b9cf3d97135c90445b889996941dd5f6bc8941a4
Michel Grootjans <michel.grootjans@gmail.com>
2026-05-11 15:00:56 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:MessTrek/StarTrek.Tests/PasswordChecker.cs

--- MessTrek/StarTrek.Tests/PasswordChecker.cs
+++ MessTrek/StarTrek.Tests/PasswordChecker.cs
@@ -21,8 +21,10 @@ public static class PasswordChecker
         
         if (requirements == PasswordRequirements.Admin)
         {
-            if (DoesNotHave(password, c => c == '!')) reasons.Add("Missing special character");
-            if (password.Last() != '!' || password.Last() != '0') reasons.Add("Missing special character");
+            List<char> specialCharacters = ['!', '0'];
+            
+            // if (DoesNotHave(password, c => c == '!')) reasons.Add("Missing special character");
+            if (!specialCharacters.Contains(password.Last())) reasons.Add("Missing special character");
         }
         
         
